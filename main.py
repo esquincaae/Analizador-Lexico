3164b7ad0a4003c97278a7a94708020192c0876c
esquincaae <edreyae20@gmail.com>
2024-02-22 22:17:48 -0600

    ahora el analizador solo acepta estructuras completas

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def procesar_entrada(entrada):
     try:
         grammar = load_grammar()
         lexer_parser = Lark(grammar, parser='lalr')
-        tree = lexer_parser.parse(entrada) #analiza la entrada y genera un árbol de análisis sintáctico (se uso en el debug, IGNORAR)
-        print(tree.pretty()) #imprime el arbol creado por la linea anterior (tambien usado para el debug, IGNORAR)
+        tree = lexer_parser.parse(entrada) 
+        #print(tree.pretty()) imprime el arbol creado por la linea anterior (tambien usado para el debug, IGNORAR)
         transformer = MyTransformer()
         transformer.transform(tree)
         return transformer.get_token_counts()
