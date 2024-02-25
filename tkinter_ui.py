aa40e868d7a88216ff980caf4747d68273894edc
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2024-02-25 16:42:09 -0600

    tu comentario de los cambios aquí

--- tkinter_ui.py
+++ tkinter_ui.py
@@ -3,18 +3,18 @@ from tkinter import messagebox
 from main import procesar_entrada  
 
 def procesar():
-    entrada = text_area_entrada.get('1.0', tk.END)  
-    try:  
+    entrada = text_area_entrada.get('1.0', tk.END)
+    try:
         token_counts = procesar_entrada(entrada)
-        text_area_resultado.delete('1.0', tk.END)  
+        text_area_resultado.delete('1.0', tk.END)
         for token_type, count in token_counts.items():
             text_area_resultado.insert(tk.END, f"Token: {token_type}, Conteo: {count}\n")
+        messagebox.showinfo("Éxito", "Entrada válida.")
     except Exception as e:
         messagebox.showerror("Error de análisis", str(e))
 
 root = tk.Tk()
 root.title("Lyra: Analizador Léxico")
-
 
 text_area_entrada = tk.Text(root, height=10, width=100)
 text_area_entrada.pack(padx=10, pady=10)
